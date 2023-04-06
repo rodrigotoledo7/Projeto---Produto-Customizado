2b18b32af8ef112f952fcce3975f3439a1ba99ea
Rodrigo Toledo <rodrigotoledo7@gmail.com>
2023-04-06 16:29:26 -0400

    ajuste default_wallpaper.png(troca de arquivo)

--- flip/AndroidProducts.mk
+++ flip/AndroidProducts.mk
@@ -6,7 +6,10 @@ PRODUCT_MAKEFILES := $(LOCAL_DIR)/parkour_flip.mk
 
 PRODUCT_SYSTEM_PROPERTIES += \
 	ro.parkour.name=Flip 
-	
+
+PRODUCT_COPY_FILES += \
+	device/parkour/flip/default_wallpaper.png:product/media/wallpapers/default_wallpaper.png
+
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.product.parkour.version=1.0 \
 	ro.config.wallpaper=product/media/wallpapers/default_wallpaper.png 
